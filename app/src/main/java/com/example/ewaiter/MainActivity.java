fa6104bb9d7133a7f02812db14ddb3ef72e469ac
psh9800 <33965103+psh9800@users.noreply.github.com>
2018-02-26 22:24:08 -0600

    This should work and have constraints. Let me know if you get a weird constraint error.

--- app/src/main/java/com/example/ewaiter/MainActivity.java
+++ app/src/main/java/com/example/ewaiter/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.ewaiter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
 
 
 
@@ -14,15 +19,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button1 = (Button) findViewById(R.id.Table1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //creating pop up view
+                PopupMenu popup = new PopupMenu(MainActivity.this, button1);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item){
+                        button1.setText(item.getTitle());
+                        return true;
+                    }
+                });
+
+                popup.show();
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.Table2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //creating pop up view
+                PopupMenu popup = new PopupMenu(MainActivity.this, button2);
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item){
+                        button2.setText(item.getTitle());
+                        return true;
+                    }
+                });
+
+                popup.show();
+            }
+        });
+    }
+
+
+
 
     }
 
-    public void perform_action(View v){
-        Button btn = (Button) findViewById(R.id.TableButton);
-        btn.setText("TEST");
 
-    }
-
-}
 
 
